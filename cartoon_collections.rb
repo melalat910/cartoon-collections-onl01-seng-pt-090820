2698bab578d5e03b7e1da2c30f2fee9d4e7a283c
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-18 20:54:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
  puts ["Doc", "Dopey", "Bashful", "Grumpy"]
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
 end
 
